6d2bcfcaa506e97de03a94cd03dbf0b67cfb31cb
piterla <jmdv6@gcloud.ua.es>
2025-12-03 19:36:15 +0100

    Cambio en el main.js para que no salte un error al cerrar la app de escritorio

--- frontend/simuflow/main.js
+++ frontend/simuflow/main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const isDev = process.env.NODE_ENV === 'development' || process.env.ELECTRON_DEV === 'true';
 
 function createWindow() {
-  const appWindow = new BrowserWindow({
+  let appWindow = new BrowserWindow({
     width: 1200,
     height: 800,
   })
